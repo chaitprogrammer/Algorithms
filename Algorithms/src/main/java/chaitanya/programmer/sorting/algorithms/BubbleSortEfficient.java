eafdd15377356c40968b9c8556bf5322fa503e66
Chaitanya <Chaitanya>
2016-08-19 18:01:54 -0400

    # WARNING: head commit changed in the meantime
    Refactoring code

--- Algorithms/src/main/java/chaitanya/programmer/sorting/algorithms/BubbleSortEfficient.java
+++ Algorithms/src/main/java/chaitanya/programmer/sorting/algorithms/BubbleSortEfficient.java
@@ -1,9 +1,13 @@
 package chaitanya.programmer.sorting.algorithms;
 
 /**
- * This program performs bubble sort on an array of integers.
+ * This program performs bubble sort on an array of integers. This is more
+ * efficients than the bubble sort program since it takes 50% less iterations in
+ * worst case.
+ * 
+ * @author Chaitanya
  */
-public class bubbleSort 
+public class BubbleSortEfficient 
 {
 	
 	public static void main(String[] args)
@@ -11,27 +15,36 @@ public class bubbleSort
 		int[] array = {9,8,7,6,5,4,3,2,1,-3,-7};
 		boolean sorted = true;
 		int loopSize = 0;
+		int length = 0;
 		int count = 0;
 		while(sorted)
 		{
 			sorted = false;
 			if(loopSize == 0)
-			loopSize = array.length-1;
+			{
+				loopSize = array.length-1;
+			}
+			else 
+			{
+				loopSize = length;
+			}
 			for(int i=0;i<loopSize;i++)
 			{
 				if(array[i] > array[i+1])
 				{
 					sort(array, i);
 					sorted = true;
+					length = i;
+					count ++;
 				}
-				count++;
 			}
 		}
 		for(int i=0;i<array.length;i++)
 		{
 			System.out.println(array[i]);
 		}
-		System.out.println("count is " + count);
+		
+		System.out.println("Number of iterations to go through :" + count);
 	}
 
 	/**
